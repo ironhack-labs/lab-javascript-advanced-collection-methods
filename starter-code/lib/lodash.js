1d3f70255a4ed033be1a14022dd64baca24d873d
Carlos Luis <clburguete@gmail.com>
2017-04-06 19:55:35 +0200

    done

--- starter-code/lib/lodash.js
+++ starter-code/lib/lodash.js
@@ -16,3 +16,22 @@ function timesFive(){
   return _.times(5, pushOpinion);
 }
 console.log(timesFive());
+
+//ITERATION3
+var goodPsswd = "1234567890";
+var badPsswd = "1123456";
+
+var noRepeatChar = function (password) {
+  var status = true;
+ for (var i = 0; i < password.length; i++) {
+   if(password.indexOf(password[i],i+1) !== -1){
+    status = false;
+   }
+ }
+ console.log(status);
+};
+noRepeatChar(badPsswd);
+
+
+noRepeatChar(goodPsswd);
+noRepeatChar(badPsswd);
