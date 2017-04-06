88e719ae81bf46bd248e38847c4bdc859c31b3ae
Livilian <olivia.sart@hotmail.com>
2017-04-06 17:15:09 +0200

    First and second steps

---
+++ starter-code/lib/theOffice.js
@@ -0,0 +1,11 @@
+var opinions = [
+  "This is the best jobever",
+  "Satisfied",
+  "At least I get paid",
+  "I'm looking for another job",
+  "I don't want to answer",
+];
+
+function randomOpinion(array) {
+  return _.sample(array);
+}
