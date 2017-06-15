52fcd4e2bd5e76489890f467ee7ca40cb433614e
sara <sara-cg@github.com>
2017-06-15 16:01:10 +0200

    first done

--- starter-code/lib/office.js
+++ starter-code/lib/office.js
@@ -1,17 +1,19 @@
-var _ = require('lodash');
-
 var opinions = [ "This is the best job ever",
                  "Satisfied",
                  "At least I get paid",
                  "I'm looking for another job",
                  "I don't want to answer"];
 
-function random () {
-  return _.sample(opinions);}
+function pickUpRandomOpinion () {
+  return _.sample(opinions);
+}
 
-function repeat (a) {
-  return _.times(a, random); }
+function pushRandomOpinions () {
+  return _.times(10, pickUpRandomOpinion)
+}
 
-var newArray = repeat(10, random (opinions));
+function repeatRandomOpinions () {
+  return _.times(5, pushRandomOpinions)
+}
 
-console.log(newArray);
+console.log(repeatRandomOpinions());
