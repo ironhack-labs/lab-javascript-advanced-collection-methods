1ba3095c2bdd658579cc0201fb29e40e3a9daec3
mikelRumayor <mikel.rumayor@gmail.com>
2017-06-15 16:02:16 +0200

    second iteration done

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -8,3 +8,21 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+
+function sortBirthdates (birthdays) {
+  return  _.chunk(birthdays, 2)
+}
+
+var nameAndDates = sortBirthdates(birthdays)
+console.log(nameAndDates)
+
+var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
+                     "Dudley Dursley", "30 June", "Tom Riddle", "31 December"];
+
+function mergeBirthdayArrays (birthdays, moreBirthdays) {
+    return _.chunk(_.concat(birthdays, moreBirthdays), 2)
+}
+
+var moreNameAndDates = mergeBirthdayArrays(birthdays, moreBirthdays)
+console.log(moreNameAndDates)
