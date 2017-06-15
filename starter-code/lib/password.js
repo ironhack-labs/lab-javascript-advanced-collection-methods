587dbc9356eeef4d0b73d4d9ce8c007677a0c5c5
Fran Bosquet <frbosquet@gmail.com>
2017-06-15 16:07:34 +0200

    Half way trought passpord.js

--- starter-code/lib/password.js
+++ starter-code/lib/password.js
@@ -4,5 +4,16 @@ var noRepeatChar = function (password) {
  return password === _.uniq(password).join('');
 };
 
-console.log(noRepeatChar(goodPsswd));
-console.log(noRepeatChar(badPsswd));
+console.log("Not repeated characters in ", goodPsswd, " is " , noRepeatChar(goodPsswd));
+console.log("Not repeated characters in ", badPsswd, " is " , noRepeatChar(badPsswd));
+
+
+var goodPsswd = "1234567890";
+var badPsswd = "1a234567890";
+var onlyNumbers = function (password) {
+ return _.without(password, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).length === 0;
+};
+onlyNumbers(goodPsswd);
+onlyNumbers(badPsswd);
+console.log("Only number with ", goodPsswd , " is ",onlyNumbers(goodPsswd));
+console.log("Only number with ", badPsswd , " is ",onlyNumbers(badPsswd));
