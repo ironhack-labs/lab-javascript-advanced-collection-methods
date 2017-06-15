a02de71695cacbe34d25e7975f95c60dc195cf1b
Marc Palatsí <mpalatsi@me.com>
2017-06-15 16:07:31 +0200

    trabajo hasta sesion de la tarde

--- starter-code/lib/office.js
+++ starter-code/lib/office.js
@@ -4,24 +4,16 @@ var opinions = [ "This is the best job ever",
                  "I'm looking for another job",
                  "I don't want to answer"];
 
-
-
-function pickUpRandomOpinion (element) {
-  var randomOpinion = _.sample(element);
-  return randomOpinion;
+function pickUpRandomOpinion() {
+  return  _.sample(opinions);
 }
 
-var collectedOpinions = [];
-
-// for (var i =0; i<10;i++) {
-//   collectedOpinions.push(pickUpRandomOpinion(opinions));
-// }
-
-_.times(10, collectedOpinions.push(pickUpRandomOpinion(opinions))
-
-console.log(collectedOpinions);
-
-
-function pushRandomOpinions (element) {
-
+function pushRandomOpinions() {
+  return _.times(10, pickUpRandomOpinion );
 }
+
+function employeeSatisfaction() {
+  return _.times(5,pushRandomOpinions);
+}
+
+console.log(employeeSatisfaction(opinions));
