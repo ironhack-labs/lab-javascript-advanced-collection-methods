eae05477e5757d0b19a2fc39cc0a11421cfcbb66
Victor Rodriguez <victor.irix@gmail.com>
2017-06-15 22:32:29 +0200

    Little Advance

--- starter-code/lib/password.js
+++ starter-code/lib/password.js
@@ -1,11 +1,24 @@
 var goodPsswd = "1234567890";
 var badPsswd = "1123456";
 
-console.log(_.difference(_.uniq(badPsswd), badPsswd));
+//console.log(_.difference(_.uniq(badPsswd), badPsswd));
 
-//var noRepeatChar = function (password) {
- //your code goes here
-//};
+
+
+var goodPsswd = "1234567890";
+var badPsswd = "1123456";
+var noRepeatChar = function(password) {
+  var uniqPassword = _.uniq(password);
+  console.log(uniqPassword);
+  var stringPassword = _.join(uniqPassword, "");
+  if (stringPassword === password) {
+    console.log("Contraseña Correcta");
+  } else {
+    console.log("Contraseña Incorrecta");
+  }
+};
+noRepeatChar(goodPsswd);
+noRepeatChar(badPsswd);
 
 //noRepeatChar(goodPsswd);
 //noRepeatChar(badPsswd);
