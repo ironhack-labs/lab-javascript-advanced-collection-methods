908b7533750f045852f993d6fdd0ef76b3324dec
Fernando Palma <ferpalma21@gmail.com>
2017-06-15 16:08:30 +0200

    3 itirate

--- starter-code/lib/office.js
+++ starter-code/lib/office.js
@@ -1,36 +1,26 @@
 var _ = require('lodash');
 
+
 var opinions = [ "This is the best job ever",
                  "Satisfied",
                  "At least I get paid",
                  "Im looking for another job",
                  "I dont want to answer"];
 
-function random(array){
-  var pickUp=_.sample(array);
+function random(){
+  var pickUp=_.sample(opinions);
   return pickUp;
 }
-
-
 console.log(random(opinions));
 
 
 function survey(){
-    var opinion=[];
-    _.times(10, function(){
-    var result = random(opinions);
-    opinion.push(result);
-    });
-    return opinion;
+    return opinion = _.times(10, random);
+
 }
+console.log(survey);
 
 function departments(){
-  var global=[];
-  _.times(5, function(){
-   var partialArray = survey();
-   global.push(partialArray);
-  });
-  return global;
+  return _.times(5, survey);
 }
-
 console.log(departments());
