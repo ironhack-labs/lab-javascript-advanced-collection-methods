4a8e7bbc9fd92d9081478a6c5e6cfb7aaed33d99
rosadl <rosadl84@gmail.com>
2017-06-15 20:37:02 +0200

    iteration 1+2

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -8,3 +8,25 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+var birthdaysCharacters = [];
+
+function generate2Darray(array){
+  birthdaysCharacters =_.chunk(array, 2);
+  return birthdaysCharacters;
+}
+
+console.log(generate2Darray(birthdays));
+
+var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
+                     "Dudley Dursley", "30 June", "Tom Riddle", "31 December"];
+
+function concat2Arrays(array1,array2){
+var array12D = generate2Darray(array1);
+var array22D = generate2Darray(array2);
+var newArray = _.concat(array12D, array22D);
+return newArray;
+}
+var newAgenda = concat2Arrays(birthdays, moreBirthdays);
+
+console.log(newAgenda);
