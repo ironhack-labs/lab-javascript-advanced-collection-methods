fb5cd784a483be80222a9155692ea1b82a5c7867
Julius Suominen <julius.suominen@gmail.com>
2017-06-15 19:49:38 +0200

    done

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -8,3 +8,32 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+                   var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
+                                        "Dudley Dursley", "30 June", "Tom Riddle", "31 December"];
+
+var pairs = [];
+
+function characterBirthdays(array) {
+  for(var i = 0; i < array.length; i += 2){
+    pairs.push(_.slice(array, i, i+2));
+  }
+  return pairs;
+};
+
+function addBirthdays(array) {
+  var chunks = _.chunk(array, 2);
+  chunks.forEach(function(item){
+    pairs.push(item);
+  });
+}
+
+function answer(){
+  characterBirthdays(birthdays);
+  addBirthdays(moreBirthdays);
+  console.log('****Below are the Harry Potter names from function calls.****');
+  console.log(pairs);
+  console.log('****Harry Potter names end here.****');
+}
+
+answer();
