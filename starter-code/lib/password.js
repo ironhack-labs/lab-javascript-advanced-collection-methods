a458bd6f72069dc05a313ec9d724d0497ffa0add
Lesly LIM <leslylim.pro@gmail.com>
2017-06-29 17:26:15 +0200

    didnt do password

---
+++ starter-code/lib/password.js
@@ -0,0 +1,10 @@
+function isPasswordCorrect(input) {
+    var dup = input;
+
+}
+
+var goodPswd = "1234567890";
+var badPswd = "1123456";
+
+console.log(isPasswordCorrect(goodPswd));
+console.log(isPasswordCorrect(badPswd));
