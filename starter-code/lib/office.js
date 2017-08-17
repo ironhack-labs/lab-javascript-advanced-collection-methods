31ddd404b7e37dfa1b6011faf72ab7c8cb088df2
ritorrehuelamo <torrehuelamo@gmail.com>
2017-08-17 16:44:37 +0200

    Iteration 1 - Done

--- starter-code/lib/office.js
+++ starter-code/lib/office.js
@@ -1,21 +1,37 @@
-var opinions = ["This is the best job ever",
+var opinions = [
+    "This is the best job ever",
     "Satisfied",
     "At least I get paid",
     "I'm looking for another job",
     "I don't want to answer"
-];
+]
 
-var randomOpinion = function(a) {
-    return _.sample(a);
+var randomOpinion = function (a) {
+    return _.sample(a)
 }
 
-var pushItem = function(b) {
-    var pushItems = [];
+var pushItem = function (a) {
+    var pushItems = []
     _.times(10, function() {
-        pushItems.push(randomOpinion(b));
-    });
-    return pushItems;
+        pushItems.push(randomOpinion(a))
+    })
+    return pushItems
 }
 
-console.log(randomOpinion(opinions));
-console.log(pushItem(opinions));
+var department = function (a) {
+  var depOp = []
+  _.times(5, function () {
+    depOp.push(pushItem(a))
+  })
+  return depOp
+}
+
+var employeeSatisfaction = function (a) {
+  var empSat = []
+  empSat.push(randomOpinion(a))
+  empSat.push(pushItem(a))
+  empSat.push(department(a))
+  return empSat
+}
+
+console.log(employeeSatisfaction(opinions))
