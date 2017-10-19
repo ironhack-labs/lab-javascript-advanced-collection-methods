f8d1a5ab075c70c884b40dfa5fa51937392fc63d
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-10-19 13:11:43 +0200

    Second iteration, first step

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -8,3 +8,7 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+var orderedBirthdays = _.chunk(birthdays, 2);
+
+console.log(orderedBirthdays);
