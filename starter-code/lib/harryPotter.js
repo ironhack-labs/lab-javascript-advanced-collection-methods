62dd6f68512af39f849ed0c4fdc1cdaa40d85797
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-19 13:37:34 +0200

    Iteration 2 with first exercise done

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
  var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
                   "Ron Weasley", "1 March", "Remus Lupin", "10 March",
                   "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
@@ -8,3 +10,9 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+function convertTwoDimArray(){
+  return _.chunk(birthdays, 2);
+}
+
+convertTwoDimArray();
