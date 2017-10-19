ee4e05ce6b2349e97a74baf7e5925618d6f03a71
gemaderus <hi@gemaderus.com>
2017-10-19 14:53:15 +0200

    Third iteration finished.

--- starter-code/lib/password.js
+++ starter-code/lib/password.js
@@ -1,17 +1,3 @@
-var _ = require('lodash');
-
-var goodPsswd = "1234567890";
-var badPsswd = "1123456";
-var noRepeatChar = function (password) {
-  var noDuplicates = _.uniq(password);
-  if(password.length !== noDuplicates.length){
-    console.log("Duplitates found");
-  }
-};
-
-noRepeatChar(goodPsswd);
-noRepeatChar(badPsswd);var _ = require('lodash');
-
 var goodPsswd = "1234567890";
 var badPsswd = "1123456";
 var noRepeatChar = function (password) {
@@ -23,3 +9,31 @@ var noRepeatChar = function (password) {
 
 noRepeatChar(goodPsswd);
 noRepeatChar(badPsswd);
+
+var goodPsswd = "1234567890";
+var badPsswd = "1a234567890";
+
+
+var onlyNumbers = function (str) {
+
+  var strSplit = _.split(str, '');
+  for(var i = 0; i < str.length; i++) {
+    if(isNaN(strSplit[i])) {
+      console.log("It is not a number");
+      break;
+    }
+  }
+};
+
+
+onlyNumbers(goodPsswd);
+onlyNumbers(badPsswd);
+
+var goodPsswd = "1234567890";
+var badPsswd = "12345678901234567890";
+
+function trimPassword(str) {
+  console.log(str.slice(0,9));
+}
+
+trimPassword(badPsswd);
