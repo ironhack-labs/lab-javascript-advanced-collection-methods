d975ef17241fa3b1efb6607e6618471ff8b7f1fe
Abraham Castro <cucharadepalo@gmail.com>
2017-10-19 13:06:00 +0200

    Segunda iteracion

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
  var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
                   "Ron Weasley", "1 March", "Remus Lupin", "10 March",
                   "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
@@ -8,3 +9,24 @@
                    "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
                    "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
                    "6 December", "Charlie Weasley", "12 December"];
+
+var names =[];
+var comp = [];
+
+function pushIn(array) {
+  array.map(function(e, i) {
+    if (i%2 === 0) {
+      comp.push(e);
+      comp.push(array[i+1]);
+      names.push(comp);
+      comp = [];
+    }
+  });
+  return names;
+}
+
+var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
+                     "Dudley Dursley", "30 June", "Tom Riddle", "31 December"];
+
+birthdays.unshift(moreBirthdays);
+pushIn(_.flatten(birthdays));
