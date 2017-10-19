62ded6f0a4a14e98f9b4ba1a80c1b426712b2f23
sdediego <sergio.dediego@outlook.com>
2017-10-19 17:58:30 +0200

    Finalizado. Completadas las 4 iteraciones

--- starter-code/lib/theOffice.js
+++ starter-code/lib/theOffice.js
@@ -1,28 +1,28 @@
 var _ = require('lodash');
 
-var opinions = [ "This is the best job ever",
-                 "Satisfied",
-                 "At least I get paid",
-                 "I'm looking for another job",
-                 "I don't want to answer"];
 
-function randomOpinion(opinions){
+var opinions = ["This is the best job ever",
+    "Satisfied",
+    "At least I get paid",
+    "I'm looking for another job",
+    "I don't want to answer"
+];
+
+function randomOpinion() {
     return _.sample(opinions);
 }
 
 function departmentOpinions() {
-  return  _.times(10, function (){
-  return randomOpinion(opinions);
-});
+    return _.times(10, function() {
+        return randomOpinion();
+    });
 }
 
-
-var employeeSatisfaction = function () {
- var array = [];
- var departmentOpinion = _.times (5, function(){
-   return departmentOpinions();
- });
- return departmentOpinion;
+var employeeSatisfaction = function() {
+    var departmentOpinion = _.times(5, function() {
+        return departmentOpinions();
+    });
+    return departmentOpinion;
 
 };
 
--- starter-code/lib/thePasswordProblem.js
+++ starter-code/lib/thePasswordProblem.js
@@ -1,10 +1,51 @@
 var _ = require('lodash');
 
+
+// Different characters
 var goodPsswd = "1234567890";
 var badPsswd = "1123456";
 var noRepeatChar = function (password) {
- //your code goes here
+    var toArray = password.split("");
+    return _.isEqual(toArray, _.uniq(toArray));
 };
 
 noRepeatChar(goodPsswd);
+console.log(noRepeatChar(goodPsswd));
 noRepeatChar(badPsswd);
+console.log(noRepeatChar(badPsswd));
+
+
+// Only numbers
+var goodPsswd = "1234567890";
+var badPsswd = "1a234567890";
+var onlyNumbers = function(password) {
+    var toArray = password.split("");
+    var onlyNumbers = _.filter(toArray, function(element) {
+        if (_.isNumber(parseInt(element)) && !isNaN(parseInt(element))) {
+            return element;
+        }
+    });
+    if (_.isEqual(toArray, onlyNumbers)) {
+        return "Valid";
+    } else {
+        return "Error";
+    }
+};
+
+onlyNumbers(goodPsswd);
+console.log(onlyNumbers(goodPsswd));
+onlyNumbers(badPsswd);
+console.log(onlyNumbers(badPsswd));
+
+
+// Ten digits only
+var goodPsswd = "1234567890";
+var badPsswd = "12345678901234567890";
+var trimPassword = function (password) {
+    return password.substring(0, 10);
+};
+
+trimPassword(goodPsswd);
+console.log(trimPassword(goodPsswd));
+trimPassword(badPsswd);
+console.log(trimPassword(badPsswd));
