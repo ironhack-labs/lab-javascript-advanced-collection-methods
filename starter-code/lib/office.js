9fb50f90e2ee52ad9122b0f8fad7ccd00046cc08
Alber Pedraza <alberpedrazaes@gmail.com>
2017-10-19 15:39:21 +0200

    Iteration2

--- starter-code/lib/office.js
+++ starter-code/lib/office.js
@@ -1,38 +1,20 @@
-var _ = require('lodash');
-
-
+//var _ = require('lodash');
 var opinions = [ "This is the best job ever",
                  "Satisfied",
                  "At least I get paid",
                  "I'm looking for another job",
-                 "I don't want to answer"
-              ];
-var opinions2 = [];
+                 "I don't want to answer"];
 
-
-function functionRandom(x) {
-  return _.sample(x);
-}
-//console.log(functionRandom(opinions));
-
-
-
-function addOpinions(x) {
-  for ( var i = 0; i <= 9; i++ ){
-    opinions2.push(functionRandom(x));
-  }
-
-  return opinions2;
+function samples () {
+  return _.sample(opinions);
 }
 
-addOpinions(opinions);
-
-
-
-function addOpinions(x) {
-  for ( var i = 0; i <= 9; i++ ){
-    opinions2.push(functionRandom(x));
-  }
-
-  return opinions2;
+function collection (){
+  return _.times(9, samples);
 }
+
+function employeeSatisfaction (){
+  return _.times(5, collection);
+}
+
+console.log(employeeSatisfaction());
