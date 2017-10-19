9115c352eb4739adb9f01cce3879514697f95b90
anajules94 <32312980+anajules94@users.noreply.github.com>
2017-10-19 15:07:16 +0200

    iteration 2 complete

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -1,18 +1,36 @@
 var _ = require('lodash');
 
- var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
-                  "Ron Weasley", "1 March", "Remus Lupin", "10 March",
-                  "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
-                   "Draco Malfoy", "5 June", "Dobby", "28 June",
-                   "Neville Longbottom", "30 July", "Harry Potter", "31 July",
-                   "Ginevra Weasley", "11 August", "Percy Weasley ", "22 August",
-                   "Hermione Granger", "19 September", "Minerva McGonagall",
-                   "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
-                   "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
-                   "6 December", "Charlie Weasley", "12 December"];
+var birthdays = ["Severus Snape", "9 January", "Arthur Weasley", "6 February",
+  "Ron Weasley", "1 March", "Remus Lupin", "10 March",
+  "Fred & George Weasley", "1 April", "Pomona Sprout", "15 May",
+  "Draco Malfoy", "5 June", "Dobby", "28 June",
+  "Neville Longbottom", "30 July", "Harry Potter", "31 July",
+  "Ginevra Weasley", "11 August", "Percy Weasley ", "22 August",
+  "Hermione Granger", "19 September", "Minerva McGonagall",
+  "4 October", "Filius Flitwick", "17 October", "Molly Weasley",
+  "30 October", "Bill Weasley", "29 November", "Rubeus Hagrid",
+  "6 December", "Charlie Weasley", "12 December"
+];
 
-function convertTwoDimArray(){
+function convertTwoDimArray() {
   return _.chunk(birthdays, 2);
 }
 
 convertTwoDimArray();
+
+var chunkedArray = [];
+
+function convertTwoDimArray() {
+  chunkedArray = _.chunk(birthdays, 2);
+  return chunkedArray;
+}
+
+convertTwoDimArray();
+
+var moreBirthdays = ["Lily Evans", "30 January", "James Potter", "27 March",
+  "Dudley Dursley", "30 June", "Tom Riddle", "31 December"
+];
+
+function joinArrays() {
+  console.log(_.concat(birthdays, moreBirthdays));
+}
