d8ed90b34dd1fa9129819b186e5c7948aa924249
Alejandro Roman <aromanarguello@gmail.com>
2017-10-22 20:45:04 -0400

    new method

--- starter-code/lib/passworditeration.js
+++ starter-code/lib/passworditeration.js
@@ -1,13 +1,20 @@
 var goodPsswd = "1234567890";
 var badPsswd = "1123456";
 var noRepeatChar = function (password) {
+
   var arrayPassword = password.split('');
+  var sortedArray = arrayPassword.sort();
 
-    console.log(arrayPassword);
+    for (var i = 0; i < sortedArray.length; i++) {
+      if ( sortedArray[i] == sortedArray[ i + 1]) {
+        console.log( 'Digit repeated' );
+      }
+      else {
+        console.log('Good Password');
+      }
 
-    arrayPassword.filter(function( item, i, ar ){
-      return ar.indexOf( item ) === i;
-    }).join('');
+    }
+
 
 };
 
