ded81e197e0d1c847162ea83741ea357a6d38056
Alejandro Roman <aromanarguello@gmail.com>
2017-10-22 21:23:52 -0400

    done with password iteration

--- starter-code/lib/passworditeration.js
+++ starter-code/lib/passworditeration.js
@@ -24,3 +24,44 @@ noRepeatChar(goodPsswd);
 
 noRepeatChar(badPsswd);
 // "Ouch, bad password."
+
+
+var goodPsswd = "1234567890";
+var badPsswd = "1a234567890";
+var onlyNumbers = function (password) {
+
+if ( isNaN( password ) ) {
+  console.log('Bad Password');
+}
+else {
+  console.log('Good Password');
+}
+
+};
+
+
+onlyNumbers(goodPsswd);
+// "Good password."
+
+onlyNumbers(badPsswd);
+// "Ouch, bad password."
+
+var goodPsswd = "1234567890";
+var badPsswd = "12345678901234567890";
+var trimPassword = function (password) {
+  var arrayPassword = password.split('');
+   var sortedArray = arrayPassword.sort();
+
+     for (var i = 0; i < sortedArray.length; i++) {
+       if ( sortedArray.length >= 10) {
+         console.log( 'Bad Password!' );
+       }
+       else {
+         console.log('Good Password');
+       }
+
+       break;
+     }
+};
+trimPassword(badPsswd);
+// "1234567890"
