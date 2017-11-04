07c28d603dbdac5faea63f8b3c528b7fe3dd3a6f
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-04 12:58:35 +0100

    feat: random answer

---
+++ starter-code/lib/exersise.js
@@ -0,0 +1,15 @@
+var responses = [ "This is the best job ever!",
+                  "Satisfied.",
+                  "At least I get paid.",
+                  "I'm looking for another job.",
+                  "I don't want to answer." ];
+
+//var people = [][];
+
+function randomResponse(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function arrayResponses() {
+
+}
