20822ee90f1d33d778a29f3d46dd24ee2057817e
Pablo Laso <plasocortabitarte@gmail.com>
2017-11-07 16:05:53 +0100

    feat: itineration 2 completed

--- starter-code/lib/exersise.js
+++ starter-code/lib/exersise.js
@@ -1,3 +1,5 @@
+//Iteration 1
+
 var responses = [ "This is the best job ever!",
                   "Satisfied.",
                   "At least I get paid.",
@@ -49,3 +51,56 @@ var employeeSatisfaction = function(array) {
 };
 
 employeeSatisfaction(responses);
+
+//Iteration 2
+
+function organizeBirthdays(array) {
+  var birthdaysArray = [];
+
+  array.reduce(function (previous, current, index) {
+    if (index % 2 != 0) {
+      birthdaysArray.push([previous, current]);
+    }
+    return current;
+  }, '');
+  return birthdaysArray;
+}
+
+var goodBirthdays = organizeBirthdays(birthdays);
+
+console.log(goodBirthdays);
+
+var moreBirthdays = [ "Lily Evans", "30 January", "James Potter", "27 March",
+                      "Dudley Dursley", "30 June", "Tom Riddle", "31 December" ];
+
+var moreGoodBirthdays = organizeBirthdays(moreBirthdays);
+
+console.log(moreGoodBirthdays);
+
+var finalBirthdaysArray = [];
+
+goodBirthdays.forEach(function(el) {
+  finalBirthdaysArray.push(el);
+});
+
+moreGoodBirthdays.forEach(function(el) {
+  finalBirthdaysArray.push(el);
+});
+
+console.log(finalBirthdaysArray);
+
+//Iteration 3
+
+var goodPsswd = "1234567890";
+var badPsswd = "1123456";
+var noRepeatChar = function (password) {
+ 
+};
+
+noRepeatChar(goodPsswd);
+
+
+noRepeatChar(badPsswd);
+
+
+//Iteration 4
