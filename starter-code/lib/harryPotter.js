5450f48814929a51838b49f04b85e7380efe7632
Eva Pavo <32174314+Evapavo@users.noreply.github.com>
2017-11-07 18:00:19 +0100

    Update harryPotter.js

--- starter-code/lib/harryPotter.js
+++ starter-code/lib/harryPotter.js
@@ -27,3 +27,41 @@ var mBirthd = moreBirthdays.reduce(function(acc,elem,index){
   
   return acc;
 },[])
+
+
+//The office//
+var responses = [ "This is the best job ever!",
+                  "Satisfied.",
+                  "At least I get paid.",
+                  "I'm looking for another job.",
+                  "I don't want to answer." ];
+
+
+  var arrays = [];
+  function randomResponse(array){
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+randomResponse(responses);
+
+function department(){
+  var answer =[];
+
+      for(var i = 0; i < 10; i++){
+          answer.push(randomResponse(responses))
+      }
+      console.log(answer)
+      
+
+  return answer;
+}
+function totalDepartment(){
+  var answerFinal = [];
+
+  for(var i= 0; i < 5; i++){
+    answerFinal.push(department())
+  }
+  console.log(answerFinal)
+  
+  
+}
